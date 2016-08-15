fd91538fdd7c3f9f6f1943f0a389bf48d37031ff
Erwin Seinen <erwin@bio-itech.nl>
2016-08-15 20:30:38 +0200

    Add hash function sample using c#

---
+++ c-sharp/hash.cs
@@ -0,0 +1,24 @@
+/// <summary>
+/// Returns hash for SubDB
+/// </summary>
+/// <param name="filePath">File Path</param>
+/// <returns>MD5 string</returns>
+string getHash(string filePath) {
+	int readSize = 64 * 1024;
+	byte[] bytes = new byte[2 * readSize];
+	using (BinaryReader reader = new BinaryReader(new FileStream(filePath, FileMode.Open)))
+	{
+		byte[] firstBytes = new byte[readSize];
+		byte[] lastBytes = new byte[readSize];
+		reader.Read(firstBytes, 0, readSize);
+		reader.BaseStream.Seek(-readSize, SeekOrigin.End);
+		reader.Read(lastBytes, 0, readSize);
+		Array.Copy(firstBytes, bytes, firstBytes.Length);
+		Array.Copy(lastBytes, 0, bytes, firstBytes.Length, lastBytes.Length);
+	}
+	var md5 = System.Security.Cryptography.MD5.Create().ComputeHash(bytes);
+	var sb = new StringBuilder();
+	foreach (var b in md5)
+		sb.Append(b.ToString("X2"));
+	return sb.ToString();
+}
